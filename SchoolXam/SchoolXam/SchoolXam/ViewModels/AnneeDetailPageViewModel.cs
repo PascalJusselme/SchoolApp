924d4575e6cad0e73c54480c3b67ab34b93b9f0e
PascalJusselme <pascal.jusselme@gmail.com>
2018-01-24 19:30:11 +0100

    Ajout MatiereDevoir Picker Prob lors du changement de Tabb

--- SchoolXam/SchoolXam/SchoolXam/ViewModels/AnneeDetailPageViewModel.cs
+++ SchoolXam/SchoolXam/SchoolXam/ViewModels/AnneeDetailPageViewModel.cs
@@ -19,7 +19,6 @@ namespace SchoolXam.ViewModels
 	public class AnneeDetailPageViewModel : BaseViewModel, INavigationAware
 	{
 		private readonly INavigationService _navigationService;
-		private readonly IEventAggregator _eventAggregator;
 
 		#region Annee Propertie and Command
 		private AnneeScolaire _annee;
@@ -98,11 +97,11 @@ namespace SchoolXam.ViewModels
 			get { return _classeAttribuable; }
 			set { SetProperty(ref _classeAttribuable, value); }
 		}
-		
+
 		#region Matiere Commands
 		public DelegateCommand<string> AddMatiereCommand { get; set; }
-		public DelegateCommand<Matiere> MatiereItemClicked { get; }
-		public DelegateCommand MatiereAttribClasseCommand { get; }
+		public DelegateCommand<Matiere> MatiereItemClicked { get; set; }
+		public DelegateCommand<Matiere> MatiereAttribClasseCommand { get; set; }
 		#endregion
 
 		#endregion
@@ -121,6 +120,13 @@ namespace SchoolXam.ViewModels
 			get { return _prenomEleve; }
 			set { SetProperty(ref _prenomEleve, value); }
 		}
+
+		//private ObservableCollection<Classe> _listClasseAttribMat;
+		//public ObservableCollection<Classe> ListClasseAttribMat
+		//{
+		//	get { return _listClasseAttribMat; }
+		//	set { SetProperty(ref _listClasseAttribMat, value); }
+		//}
 		#endregion
 
 		public AnneeDetailPageViewModel(
@@ -146,7 +152,7 @@ namespace SchoolXam.ViewModels
 			#region MatiereCommand
 			MatiereItemClicked = new DelegateCommand<Matiere>(DoMatiereClicked);
 			AddMatiereCommand = new DelegateCommand<string>(AddMatiere);
-			MatiereAttribClasseCommand = new DelegateCommand(AttribMatiereClasse);
+			MatiereAttribClasseCommand = new DelegateCommand<Matiere>(AttribMatiereClasse);
 			#endregion
 		}
 
@@ -192,7 +198,7 @@ namespace SchoolXam.ViewModels
 				Title = "Nouvelle Année";
 			}
 		}
-		
+
 		public override void Destroy()
 		{
 
@@ -310,7 +316,7 @@ namespace SchoolXam.ViewModels
 				Matieres = new List<Matiere>(),
 				Eleves = new ObservableCollection<Eleve>()
 			};
-			
+
 			if (IsValidClasse(classe))
 			{
 				// Ajout de la Classe a la liste des Classes de l'Annee
@@ -395,6 +401,8 @@ namespace SchoolXam.ViewModels
 				}
 			}
 
+			//ListClasseAttribMat = new ObservableCollection<Classe>(matiere.Classes);
+
 			return matiere;
 		}
 
@@ -423,9 +431,9 @@ namespace SchoolXam.ViewModels
 								   .Exists(ma => ma.matiereLib == matiere.matiereLib));
 		}
 
-		private void AttribMatiereClasse()
+		private void AttribMatiereClasse(Matiere matiere)
 		{
-			Matiere.Classes.Clear();
+			matiere.Classes.Clear();
 
 			foreach (var data in ClasseAttribuable)
 			{
@@ -433,18 +441,18 @@ namespace SchoolXam.ViewModels
 
 				if (data.Selected)
 				{
-					Matiere.Classes.Add(data.Data);
+					matiere.Classes.Add(data.Data);
 
-					if (!classe.Matieres.Contains(Matiere))
+					if (!classe.Matieres.Contains(matiere))
 					{
-						classe.Matieres.Add(Matiere);
+						classe.Matieres.Add(matiere);
 					}
 				}
 				else
 				{
-					if (classe.Matieres.Contains(Matiere))
+					if (classe.Matieres.Contains(matiere))
 					{
-						classe.Matieres.Remove(Matiere);
+						classe.Matieres.Remove(matiere);
 					}
 				}
 
@@ -454,10 +462,12 @@ namespace SchoolXam.ViewModels
 				}
 			}
 
-			if (Matiere.matiereID != 0)
+			if (matiere.matiereID != 0)
 			{
-				_rep.UpdateMatiere(Matiere);
+				_rep.UpdateMatiere(matiere);
 			}
+
+			//ListClasseAttribMat = new ObservableCollection<Classe>(matiere.Classes);
 		}
 
 		private async void DoMatiereClicked(Matiere matiere)
